951c248569520507b5335c6cb9d060d956592d7b
Scala Steward <me@scala-steward.org>
2021-07-28 17:43:15 +0200

    Applied Scalafix rule(s) https://raw.githubusercontent.com/scalatest/autofix/e4de53fa40fac423bd64d165ff36bde38ce52388/3.1.x/rules/src/main/scala/org/scalatest/autofix/v3_1_x/RewriteDeprecatedNames.scala
    See https://github.com/scalatest/autofix/blob/master/3.1.x/README.md for details

--- src/test/scala/com/advancedtelematic/util/TreeHubSpec.scala
+++ src/test/scala/com/advancedtelematic/util/TreeHubSpec.scala
@@ -3,8 +3,9 @@ package com.advancedtelematic.util
 import com.advancedtelematic.libats.data.DataType.Namespace
 import com.advancedtelematic.treehub.Settings
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-abstract class TreeHubSpec extends FunSuite with Matchers with ScalaFutures with Settings {
+abstract class TreeHubSpec extends AnyFunSuite with Matchers with ScalaFutures with Settings {
   val defaultNs = Namespace("default")
 }
