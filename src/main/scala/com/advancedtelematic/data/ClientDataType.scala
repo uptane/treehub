50b2feb84e0ec30380382ccedef3d68271c405a8
Raigi Gläser <raigi.glaeser@toradex.com>
2023-12-13 14:01:03 +0100

    Move imports to higher scope in ClientDataType

--- src/main/scala/com/advancedtelematic/data/ClientDataType.scala
+++ src/main/scala/com/advancedtelematic/data/ClientDataType.scala
@@ -3,14 +3,14 @@ package com.advancedtelematic.data
 import com.advancedtelematic.libats.messaging_datatype.DataType.Commit
 
 object ClientDataType {
+
+  import io.circe.{Decoder, Encoder}
+  import com.advancedtelematic.libats.codecs.CirceRefined.*
+  import io.circe.generic.semiauto.{deriveEncoder, deriveDecoder}
+
   case class StaticDelta(from: Commit, to: Commit, size: Long)
 
   object StaticDelta {
-
-    import io.circe.{Decoder, Encoder}
-    import com.advancedtelematic.libats.codecs.CirceRefined.*
-    import io.circe.generic.semiauto.{deriveEncoder, deriveDecoder}
-
     implicit val Encoder: Encoder[StaticDelta] = deriveEncoder[StaticDelta]
     implicit val Decoder: Decoder[StaticDelta] = deriveDecoder[StaticDelta]
   }
